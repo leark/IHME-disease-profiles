e5a54bdb18708ef9ddd7ad1604520743d5aa080f
Sarah Park <parks96@uw.edu>
2018-02-09 18:50:22 -0800

    search complete

---
+++ Feb9_frontend/search.php
@@ -0,0 +1,47 @@
+<?php
+
+	// How to connect this PHP file to HTML
+	// in action attribute of form/input, added 'search.php'
+	
+	// How to pass PHP variables to Javascript
+	// https://stackoverflow.com/questions/23740548/how-to-pass-variables-and-data-from-php-to-javascript
+	
+	// Database configurations -- connected to AWS RDS
+	$dbhost = "ihme.c1blkxjim6dl.us-west-2.rds.amazonaws.com";
+	$dbport = 3306;
+	$dbname = "ihmedb";
+	$username = "info490user";
+	$password = "mypassword";
+	
+	try {
+		// If the input isn't empty
+		if (!empty($_GET["causeName"])) {
+			// Change the name attribute to align in both HTML and here
+			// Get the value in the input
+			$causeName = $_GET["causeName"];
+			
+			// Database connection
+			$conn = new PDO("mysql:host=$dbhost;port=$dbport; dbname=$dbname", $username, $password) 
+						or die("Could not connect: " . mysql.error());
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+			
+			// Prints the JSON result of executeQuery
+			print_r(executeQuery($conn, $causeName));
+		}
+	} catch(PDOException $e) {
+		// Either connection failed or there was an error in the query
+		echo "Connection failed: " . $e->getMessage();
+	}
+	
+	// $value is cause_name like Meningitis, $conn is the database connection
+	// Utilized PDO (prepare()) to prevent SQL Injection
+	// Returns the query as a JSON value
+	function executeQuery($conn, $value) {
+		$cause = $value;
+		$stmt = $conn->prepare("SELECT * FROM DEATHS WHERE cause_name LIKE :cause");
+		$stmt->bindParam(':cause', $cause);
+		$stmt->execute();
+		$result = $stmt->fetchAll();
+		return json_encode($result); // returns the row as JSON
+	}
+?>
