a3c3e502b4387fb6a88791a1cb2cc5328f0dcd18
parks96@uw.edu <parks96@uw.edu>
2018-03-01 20:51:29 -0800

    source country

---
+++ Demo_Mar2/php/finance.php
@@ -0,0 +1,32 @@
+<?php	
+	try {
+		if (!empty($_GET["locationName"])) { // if input isn't required
+
+			$locationName = $_GET["locationName"];
+			$locationName = str_replace(' ', '_', $locationName);
+			$conn = getConnection(); // database connection
+			
+			echo json_encode(getCountryContributions($conn, $locationName));
+		}
+	} catch(PDOException $e) {
+		// Either connection failed or there was an error in the query
+		echo "Connection failed: " . $e->getMessage();
+	}
+	
+	function getConnection() {
+		include 'dbconfig.php';
+		$connection = new PDO("mysql:host=$dbhost;port=$dbport; dbname=$dbname", $username, $password) 
+						or die("Could not connect: " . mysql.error());
+		$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		return $connection;
+	}
+
+	function getCountryFunds($conn, $location) {
+		$stmt = $conn->prepare("SELECT * FROM tbl_FINANCE WHERE recipient_country LIKE :location 
+										ORDER BY year DESC");
+		$stmt->bindParam(':location', $location);
+		$stmt->execute();
+		$result = $stmt->fetchAll();
+		return $result; // returns the row as array	
+	}
+?>
