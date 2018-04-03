367c76ff879b510c9cb43c66cd79e7c9e2534dfa
parks96@uw.edu <parks96@uw.edu>
2018-04-02 20:21:48 -0700

    added more countries and causes

---
+++ Demo_Apr6/js/select2.js
@@ -0,0 +1,12 @@
+$(document).ready(function() { 
+	$("#e1").select2({
+		placeholder: "Select Cause",
+		allowClear: true,
+		width: '30%'
+	}); 
+	$("#e2").select2({
+		placeholder: "Select Location",
+		allowClear: true,
+		width: '25%'
+	}); 
+});
