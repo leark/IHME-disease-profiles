494ad6e5e8f2d987807c763ad5b147e8fab69d9a
parks96@uw.edu <parks96@uw.edu>
2018-04-22 00:23:33 -0700

    initial draft

--- Demo_Apr20/js/heat_map.js
+++ Demo_Apr20/js/heat_map.js
@@ -6,59 +6,51 @@ $(function() {
 		data: {request_type:"heat_rank", causeName: cause_name, locationName: location_name}
 	}).done(function (response) {
 		var rows = JSON.parse(response);
-		console.log(rows);
+		// console.log(rows);
 
 		// Title
-		$('#heatTitle').text(`Heat map of ${cause_name}`);
-		
+		$('#heatTitle').text(`Comparisons of ${cause_name}`);
+
 		// formatting
-		var itemSize = 22,
+		let itemSize = 51
 			cellSize = itemSize - 1,
 			margin = {top: 120, right: 20, bottom: 20, left: 110};
-		  
-		var width = 750 - margin.right - margin.left,
-			height = 300 - margin.top - margin.bottom;
 
-		var formatDate = d3.time.format("%Y-%m-%d");
-		
+		let width = 750 - margin.right - margin.left,
+			height = 500 - margin.top - margin.bottom;
+
 		// data
-		var x_elements = d3.set(rows.map(function( rows ) { return rows.measure; } )).values(),
+		let x_elements = d3.set(rows.map(function( rows ) { return rows.measure; } )).values(),
 			y_elements = d3.set(rows.map(function( rows ) { return rows.location; } )).values();
 
-		var xScale = d3.scale.ordinal()
+		let xScale = d3.scale.ordinal()
 			.domain(x_elements)
 			.rangeBands([0, x_elements.length * itemSize]);
 
-		var xAxis = d3.svg.axis()
+		let xAxis = d3.svg.axis()
 			.scale(xScale)
-			// .tickFormat(function (d) {
-				// return d;
-			// })
 			.orient("top");
 
-		var yScale = d3.scale.ordinal()
+		let yScale = d3.scale.ordinal()
 			.domain(y_elements)
 			.rangeBands([0, y_elements.length * itemSize]);
 
-		var yAxis = d3.svg.axis()
+		let yAxis = d3.svg.axis()
 			.scale(yScale)
-			// .tickFormat(function (d) {
-				// return d;
-			// })
 			.orient("left");
 
-		var colorScale = d3.scale.threshold()
-			.domain([1, 4])
-			.range(["#2980B9", "#E67E22", "#27AE60", "#27AE60"]);
+		// var colorScale = d3.scale.threshold()
+		// 	.domain([1, 4])
+		// .range(["#912711", "#002a53", "#27AE60", "#27AE60"]);
 
-		var svg = d3.select('#heatmap')
+		let svg = d3.select('#heatmap')
 			.append("svg")
 			.attr("width", width + margin.left + margin.right)
 			.attr("height", height + margin.top + margin.bottom)
 			.append("g")
 			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-		var cells = svg.selectAll('rect')
+		let cells = svg.selectAll('rect')
 			.data(rows)
 			.enter().append('g').append('rect')
 			.attr('class', 'cell')
@@ -66,7 +58,17 @@ $(function() {
 			.attr('height', cellSize)
 			.attr('y', function(d) { return yScale(d.location); })
 			.attr('x', function(d) { return xScale(d.measure); })
-			.attr('fill', function(d) { return colorScale(d.val); });
+			// .attr('fill', function(d) { return colorScale(d.val); });
+			.attr('stroke', 'black')
+			.attr('fill', 'white');
+
+		let boxes = svg.selectAll('text')
+			.data(rows)
+			.enter().append('g').append('text')
+			.style('text-anchor', 'middle')
+			.text(function(d) { return Math.round( d.val * 10 ) / 10})
+			.attr('x', function(d) { return xScale(d.measure) + 20; })
+			.attr('y', function(d) { return yScale(d.location) + 30; });
 
 		svg.append("g")
 			.attr("class", "y axis")
@@ -87,13 +89,13 @@ $(function() {
 			});
 
 		// Footer
-		var containerDiv = document.getElementById("rankTableDiv");
-		var footer = document.createElement("p");
-		var footer_text = document.createTextNode("2016, age-standardized, rate");
+		let containerDiv = document.getElementById("heatDiv");
+		let footer = document.createElement("p");
+		let footer_text = document.createTextNode("2016, age-standardized, rate");
 		footer.appendChild(footer_text);
 		containerDiv.appendChild(footer);
-		
-	}).fail(function (e) { 
+
+	}).fail(function (e) {
 		console.log(e);
 	});
 });
